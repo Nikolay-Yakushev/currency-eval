c16ac172f52e9921f04d3802a074be13d512010a
nyakushev <n.yakushev@palabra.ai>
2024-12-08 21:39:06 +0300

    upd deployment

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -23,7 +23,7 @@ func main() {
 	absPath, _ := filepath.Abs(".")
 	log.Println("Current working directory", zap.String("path", absPath))
 
-	conf, err := NewConf(".")
+	conf, err := NewConf(absPath)
 	if err != nil {
 		log.Fatalf("Failed to launch app config %v", err)
 	}
